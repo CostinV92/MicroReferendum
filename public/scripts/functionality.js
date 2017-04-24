ec613f7654a52079054ec1bc40326b39140baff9
Irina <Irina Botez>
2017-04-25 02:24:01 +0300

    Fixed css issue with duplicate radio buttons

--- public/scripts/functionality.js
+++ public/scripts/functionality.js
@@ -24,7 +24,7 @@ function filter_by_county(id) {
   	});
 
   	polls = put_id($.parseJSON(data));
-  	var html; = '<ol>';
+  	var html = '<ol>';
   	for (var i = 0; i < polls.length; ++i) {
 		html += '<li id="'+ polls[i].id + '"><h4>' + polls[i].subject + '</h4></li>';
 		html += '<a href="#pollPopup" data-rel="popup" onclick="create_poll('+ "'" + polls[i].id + "'" +')">';
@@ -152,3 +152,4 @@ function poll_info()
 		}
 	});
 }
+
