32f2d28cc717a248fe584e5bb47c14d74dc689c0
Maria Petre <petremariaaa@gmail.com>
2017-05-23 12:21:07 +0300

    Moved functions in new controller

--- public/scripts/functionality.js
+++ public/scripts/functionality.js
@@ -1,7 +1,6 @@
 //Variabile
-var polls, filterList = [], idx = 0, pollsListFiltred = [];
+
 var currentDate = new Date();
-var countyList = ['Arad', 'Arges','Bacau','Bihor','Bistrita-Nasaud'];
 //login
 function login_info() {
 	var username = $('#username').val();
@@ -14,82 +13,6 @@ function login_info() {
 }
 //end Login
 //harta
-
-
-//Category Filter
-function add_cat(id) {
-	if (filterList.indexOf(id) == -1){
-		filterList[idx] = id
-		++idx;
-	} else {
-		for( var k = 0; k < filterList.length; ++k){
-			if (filterList[k] === id) {
-				filterList.splice(k,1);
-				--idx;
-			}
-		}
-	}
-}
-
-function create_polls_list() {
-	put_id(polls);//trebuie scos dupa ce vin bine datele de la server
-	var html = '<ol>';
-	var p = 0;
-	var index = 0;
-	for (var i = 0; i < polls.length; ++i) {
-		for(var j = 0; j < filterList.length; ++j)
-			if (polls[i].category === filterList[j]) {
-				html += '<li id="'+ polls[i].id + '"><h4>' + polls[i].subject + '</h4></li>';
-				html += '<a href="#pollPopup" data-rel="popup" onclick="create_poll('+ "'" + polls[i].id + "'" +')">';
-				html += '<p>' + polls[i].description + '</p>';
-				html += '</a>';
-				pollsListFiltred[p++] = polls[i];
-			}
-	}
-	html += '</ol>';
-	$("div#poll-list").html(html);
-}
-//in popup
-function create_poll(id) {
-	var html = '<div>';
-	for (var i = 0; i < pollsListFiltred.length; ++i) 
-		if (pollsListFiltred[i].id === id) {
-			html += '<h4>' + pollsListFiltred[i].subject + '</h4>';			
-			html += '<p>' + pollsListFiltred[i].description + '</p>';
-			pollsListFiltred[i].endDate = new Date(pollsListFiltred[i].endDate);
-			if (pollsListFiltred[i].endDate >= currentDate) {				
-				html += '<p style="color:red;">Mai sunt <b>' + from_milis_to_days(pollsListFiltred[i].endDate - currentDate) + '</b> zile pana la inchiderea votului.</p>';
-				html += '<p>Sunteti de acord?</p>';
-				html += '<div id="vote">';
-				html += '<button id="da" class="btn btn-default" style="width:50%" onclick="submit_vote(id)">DA</button>';
-				html += '<button id="nu" class="btn btn-default" style="width:50%" onclick="submit_vote(id)">NU</button>';
-				html += '</div>';
-			} else { 
-				html += '<p>Votul s-a incheiat.</p>';
-				html += '<span>Rezultatul votului este:' + /*procent*/ '100% DA -' /*procent*/ + '0% NU</span><br>';
-			}
-			
-		}
-	$("div#poll-vote").html(html);
-}
-function from_milis_to_days(date) {
-	//86400000 milisecounds in a day
-	var days = Math.round(date / 86400000);
-	console.log(days);
-	return days;
-}
-
-function submit_vote(id) {
-	var vot;
-	if (id === 'da')
-		vot = 0;
-	else
-		vot = 1;
-	$("div#vote").html('<p class="text-center"><b>Va multumim!</b></p>');
-	$.post('votes/add', function(vot){
-		console.log('Vot trimis cu succes.');
-	});
-}
 
 //poll form data
 function poll_info()
--- public/scripts/getPollsData.js
+++ public/scripts/getPollsData.js
@@ -1,3 +1,5 @@
+var polls, filterList = [], idx = 0, pollsListFiltred = [];
+
 $( document ).ready(function() {
 
     $.urlParam = function(name) {
@@ -16,12 +18,12 @@ $( document ).ready(function() {
     };
 
     function draw_data(data) {
-        polls = put_id(data);
+        polls = data;
         var html = '<ol>';
         var p = 0;
         for (var i = 0; i < polls.length; ++i) {
-            html += '<li id="'+ polls[i].id + '"><h4>' + polls[i].subject + '</h4></li>';
-            html += '<a href="#pollPopup" data-rel="popup" onclick="create_poll('+ "'" + polls[i].id + "'" +')">';
+            html += '<li id="'+ polls[i]._id + '"><h4>' + polls[i].subject + '</h4></li>';
+            html += '<a href="#pollPopup" data-rel="popup" onclick="create_poll('+ "'" + polls[i]._id + "'" +')">';
             html += '<p>' + polls[i].description + '</p>';
             html += '</a>';
             pollsListFiltred[p++] = polls[i];
@@ -30,12 +32,88 @@ $( document ).ready(function() {
         $('div#poll-list').html(html);
     }
 
-    function put_id(data) {
-        var index = 0;
-        for (var i = 0; i < data.length; ++i) 
-            data[i].id = "poll-no-" + index++;
-        return data;
-    }
-
     filter_by_county($.urlParam('region'));
+
 });
+
+//Category Filter
+function add_cat(id) {
+    if (filterList.indexOf(id) == -1){
+        filterList[idx] = id
+        ++idx;
+    } else {
+        for( var k = 0; k < filterList.length; ++k){
+            if (filterList[k] === id) {
+                filterList.splice(k,1);
+                --idx;
+            }
+        }
+    }
+}
+function checked_poll() {
+    for(var i = 0; i < polls.length; ++i)
+        polls[i].checked = false;
+}
+
+function create_polls_list() {
+    var html = '<ol>';
+    var p = 0;
+    var index = 0;
+    checked_poll();
+    for (var i = 0; i < polls.length; ++i) {
+        for(var j = 0; j < filterList.length; ++j)
+            for(var k = 0; k < polls[i].tags.length; ++k)
+                if (polls[i].tags[k] === filterList[j] && !polls[i].checked) {
+                    polls[i].checked = true;
+                    html += '<li id="'+ polls[i]._id + '"><h4>' + polls[i].subject + '</h4></li>';
+                    html += '<a href="#pollPopup" data-rel="popup" onclick="create_poll("'+  polls[i]._id +'")">';
+                    html += '<p>' + polls[i].description + '</p>';
+                    html += '</a>';
+                    pollsListFiltred[p++] = polls[i];
+                }
+    }
+    html += '</ol>';
+    $("div#poll-list").html(html);
+}
+//in popup
+function create_poll(id) {
+    var html = '<div>';
+    checked_poll();
+    for (var i = 0; i < pollsListFiltred.length; ++i) 
+        if (pollsListFiltred[i]._id === id) {
+            html += '<h4>' + pollsListFiltred[i].subject + '</h4>';         
+            html += '<p>' + pollsListFiltred[i].description + '</p>';
+            pollsListFiltred[i].endDate = new Date(pollsListFiltred[i].endDate);
+            if (pollsListFiltred[i].endDate >= currentDate) {               
+                html += '<p style="color:red;">Mai sunt <b>' + from_milis_to_days(pollsListFiltred[i].endDate - currentDate) + '</b> zile pana la inchiderea votului.</p>';
+                html += '<p>Sunteti de acord?</p>';
+                html += '<div id="vote">';
+                html += '<button id="da" class="btn btn-default" style="width:50%" onclick="submit_vote(id)">DA</button>';
+                html += '<button id="nu" class="btn btn-default" style="width:50%" onclick="submit_vote(id)">NU</button>';
+                html += '</div>';
+            } else { 
+                html += '<p>Votul s-a incheiat.</p>';
+                html += '<span>Rezultatul votului este:' + /*procent*/ '100% DA -' /*procent*/ + '0% NU</span><br>';
+            }
+            
+        }
+    $("div#poll-vote").html(html);
+}
+
+function from_milis_to_days(date) {
+    //86400000 milisecounds in a day
+    var days = Math.round(date / 86400000);
+    return days;
+}
+
+function submit_vote(id) {
+    var vot;
+    if (id === 'da')
+        vot = 0;
+    else
+        vot = 1;
+    $("div#vote").html('<p class="text-center"><b>Va multumim!</b></p>');
+    $.post('votes/add', function(vot){
+        console.log('Vot trimis cu succes.');
+    });
+}
