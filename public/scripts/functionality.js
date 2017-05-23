33671f46951fbf07e9a5155db2a2d8822ef2ccdf
Victor Gabriel <costinv92@gmail.com>
2017-05-23 23:45:27 +0300

    Move javascript from views to functionality.js
    Signed-off-by: Victor Gabriel <costinv92@gmail.com>

--- public/scripts/functionality.js
+++ public/scripts/functionality.js
@@ -44,3 +44,40 @@ function poll_info()
 	});
 }
 
+// register
+function Captcha(){
+	var alpha = new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+	var i;
+	for (i=0;i<6;i++){
+		var a = alpha[Math.floor(Math.random() * alpha.length)];
+		var b = alpha[Math.floor(Math.random() * alpha.length)];
+		var c = alpha[Math.floor(Math.random() * alpha.length)];
+		var d = alpha[Math.floor(Math.random() * alpha.length)];
+		var e = alpha[Math.floor(Math.random() * alpha.length)];
+		var f = alpha[Math.floor(Math.random() * alpha.length)];
+		var g = alpha[Math.floor(Math.random() * alpha.length)];
+	}
+	var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' '+ f + ' ' + g;
+	document.getElementById("mainCaptcha").value = code;
+}
+function ValidCaptcha(){
+	var string1 = removeSpaces(document.getElementById('mainCaptcha').value);
+	var string2 = removeSpaces(document.getElementById('txtInput').value);
+	if (string1 == string2){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+function removeSpaces(string){
+	return string.split(' ').join('');
+}
+function Hide1() {
+	document.getElementById("field1").style.display = "block";
+	document.getElementById("fileset0").style.display = "none";
+	document.getElementById("field2").style.display = "block";
+	document.getElementById("field3").style.display = "block";
+	Captcha();
+}
+// end register
